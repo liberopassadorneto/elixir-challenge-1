aec15d90fb14f54d968f4f60216e3e96bc29b2ce
Libero Passador Neto <libero.neto@vitta.me>
2024-03-23 20:13:49 -0300

    feat: add count_list module

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,16 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  defp count_list([], acc), do: acc
+
+  defp count_list([_ | tail], acc) do
+    acc = acc + 1
+    count_list(tail, acc)
   end
+
+  def call(list), do: count_list(list, 0)
 end
+
+# [1, 3, 6, 43, 6]
+# 1) [3,6,43,6] ; acc=0+1=1 ; count_list([3,6,43,6], 1)
+# 2) [6,43,6] ; acc=1+1=2 ; count_list([6,43,6], 2)
+# 3) [43,6] ; acc=2+1=3 ; count_list([43,6], 3)
+# ...
